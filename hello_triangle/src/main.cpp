2043e51a4ba3a961b1ad2c0ce02a7af3ce1b66c1
hopeless programmer <me@hopeless-programmer.online>
2022-04-12 17:21:09 +0300

    Add glfw

--- hello_triangle/src/main.cpp
+++ hello_triangle/src/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 
+#include <GLFW/glfw3.h>
+
 int main() {
+    try {
+        if (!glfwInit()) throw std::runtime_error("GLFW initialization failed.");
+
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+        const auto window = glfwCreateWindow(800, 600, "Hello triangle", nullptr, nullptr);
+
+        if (!window) throw std::runtime_error("Window creation failed.");
+
+        while (!glfwWindowShouldClose(window))
+        {
+            glfwPollEvents();
+        }
+
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+    catch (const std::exception error) {
+        std::cerr << error.what() << std::endl;
+
+        throw;
+    }
+
     std::cout << "done" << std::endl;
 
     return 0;
--- readme.md
+++ readme.md
@@ -3,3 +3,7 @@
 - `cmake -G "MinGW Makefiles" -S "." -B "./_build"` to generate.
 - `cmake --build "./_build"` to build.
 - `_build\hello_triangle\hello_triangle.exe` to run `hello_triangle`.
+
+## Dependencies
+
+- [GLFW](https://www.glfw.org/).
