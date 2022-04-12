cac8b4868f7e8f6d840384b7ac92d0dfe63b8ccd
hopeless programmer <me@hopeless-programmer.online>
2022-04-12 22:38:53 +0300

    Update main.cpp

--- hello_triangle/src/main.cpp
+++ hello_triangle/src/main.cpp
@@ -1,7 +1,33 @@
+#include <vector>
 #include <iostream>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+struct Vertex {
+    float x;
+    float y;
+};
+
+std::string VERTEX_SHADER_SOURCE = R"(#version 450
+layout (location = 0) in vec2 position;
+
+void main() {
+    gl_Position = vec4(position, 0, 1);
+}
+)";
+const char* VERTEX_SHADER_SOURCES[] = { VERTEX_SHADER_SOURCE.c_str() };
+const GLint VERTEX_SHADER_LENGTHS[] = { static_cast<GLint>(VERTEX_SHADER_SOURCE.length()) };
+
+std::string FRAGMENT_SHADER_SOURCE = R"(#version 450
+layout (location = 0) out vec4 color;
+
+void main() {
+    color = vec4(1);
+}
+)";
+const char* FRAGMENT_SHADER_SOURCES[] = { FRAGMENT_SHADER_SOURCE.c_str() };
+const GLint FRAGMENT_SHADER_LENGTHS[] = { static_cast<GLint>(FRAGMENT_SHADER_SOURCE.length()) };
 
 int main() {
     try {
@@ -20,6 +46,94 @@ int main() {
 
         if (glewInit() != GLEW_OK) throw std::runtime_error("GLEW initialization failed.");
 
+        const auto vertices = std::vector<Vertex>{
+            { -0.5f, -0.5f, },
+            { +0.5f, -0.5f, },
+            { +0.0f, +0.5f, },
+        };
+
+        GLuint vertexBuffer;
+
+        glCreateBuffers(1, &vertexBuffer);
+        glNamedBufferStorage(vertexBuffer, sizeof(Vertex) * vertices.size(), vertices.data(), 0);
+
+        GLuint attributesBuffer;
+
+        glCreateVertexArrays(1, &attributesBuffer);
+        glVertexArrayVertexBuffer(attributesBuffer, 0, vertexBuffer, 0, sizeof(Vertex));
+        glVertexArrayAttribFormat(attributesBuffer, 0, 3, GL_FLOAT, GL_FALSE, 0);
+        glEnableVertexArrayAttrib(attributesBuffer, 0);
+
+        const auto vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+        glShaderSource(vertexShader, 1, VERTEX_SHADER_SOURCES, VERTEX_SHADER_LENGTHS);
+        glCompileShader(vertexShader);
+
+        GLint vertexShaderCompileStatus;
+
+        glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &vertexShaderCompileStatus);
+
+        if (vertexShaderCompileStatus != GL_TRUE) {
+            GLint size = 0;
+
+            glGetShaderiv(vertexShader, GL_INFO_LOG_LENGTH, &size);
+
+            std::string log;
+
+            log.resize(size);
+            glGetShaderInfoLog(vertexShader, size, &size, log.data());
+
+            throw std::runtime_error(log);
+        }
+
+        const auto fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+        glShaderSource(fragmentShader, 1, FRAGMENT_SHADER_SOURCES, FRAGMENT_SHADER_LENGTHS);
+        glCompileShader(fragmentShader);
+
+        GLint fragmentShaderCompileStatus;
+
+        glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &fragmentShaderCompileStatus);
+
+        if (fragmentShaderCompileStatus != GL_TRUE) {
+            GLint size = 0;
+
+            glGetShaderiv(fragmentShader, GL_INFO_LOG_LENGTH, &size);
+
+            std::string log;
+
+            log.resize(size);
+            glGetShaderInfoLog(fragmentShader, size, &size, log.data());
+
+            throw std::runtime_error(log);
+        }
+
+        const auto program = glCreateProgram();
+
+        glAttachShader(program, vertexShader);
+        glAttachShader(program, fragmentShader);
+        glLinkProgram(program);
+
+        GLint linkStatus;
+
+        glGetProgramiv(program, GL_LINK_STATUS, &linkStatus);
+
+        if (linkStatus != GL_TRUE) {
+            GLint size = 0;
+
+            glGetProgramiv(program, GL_INFO_LOG_LENGTH, &size);
+
+            std::string log;
+
+            log.resize(size);
+            glGetProgramInfoLog(program, size, &size, log.data());
+
+            throw std::runtime_error(log);
+        }
+
+        glDeleteShader(vertexShader);
+        glDeleteShader(fragmentShader);
+
         while (!glfwWindowShouldClose(window))
         {
             glfwPollEvents();
@@ -31,11 +145,30 @@ int main() {
             glViewport(0, 0, width, height);
             glClearColor(1.0f, 0.5f, 0.0f, 1.0f);
             glClear(GL_COLOR_BUFFER_BIT);
+            glBindVertexArray(attributesBuffer);
+            glDrawArrays(GL_TRIANGLES, 0, 3);
+            glFlush();
 
             glfwSwapBuffers(window);
         }
 
-        if (glGetError() != GL_NO_ERROR) throw std::runtime_error("OpenGL error.");
+        glDeleteProgram(program);
+        glDeleteVertexArrays(1, &attributesBuffer);
+        glDeleteBuffers(1, &vertexBuffer);
+
+        auto error = glGetError();
+
+        if (error != GL_NO_ERROR) {
+            if (error == GL_INVALID_ENUM) throw std::runtime_error("GL_INVALID_ENUM");
+            if (error == GL_INVALID_VALUE) throw std::runtime_error("GL_INVALID_VALUE");
+            if (error == GL_INVALID_OPERATION) throw std::runtime_error("GL_INVALID_OPERATION");
+            if (error == GL_INVALID_FRAMEBUFFER_OPERATION) throw std::runtime_error("GL_INVALID_FRAMEBUFFER_OPERATION");
+            if (error == GL_OUT_OF_MEMORY) throw std::runtime_error("GL_OUT_OF_MEMORY");
+            if (error == GL_STACK_UNDERFLOW) throw std::runtime_error("GL_STACK_UNDERFLOW");
+            if (error == GL_STACK_OVERFLOW) throw std::runtime_error("GL_STACK_OVERFLOW");
+
+            throw std::runtime_error("Unknown OpenGL error: " + std::to_string(error) + ".");
+        }
 
         glfwDestroyWindow(window);
         glfwTerminate();
