4c167b6fc37204727d3f0f16c2c57b96bbe28bae
hopeless programmer <me@hopeless-programmer.online>
2022-04-13 15:28:03 +0300

    Minor fixes

--- hello_triangle/src/main.cpp
+++ hello_triangle/src/main.cpp
@@ -148,6 +148,7 @@ int main() {
             glClear(GL_COLOR_BUFFER_BIT);
             glBindVertexArray(attributesBuffer);
             glUseProgram(program);
+            glValidateProgram(program);
 
             GLint validateStatus;
 
