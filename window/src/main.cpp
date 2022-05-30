21c08520b646e7700d5c16cc8b4c2329f85f2737
hopeless programmer <me@hopeless-programmer.online>
2022-05-30 03:02:05 +0300

    Add window projecct

---
+++ window/src/main.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+#include <GLFW/glfw3.h>
+
+int main() {
+    try {
+        if (!glfwInit()) throw std::runtime_error("GLFW initialization failed.");
+
+        const auto window = glfwCreateWindow(800, 600, "Hello triangle", nullptr, nullptr);
+
+        if (!window) throw std::runtime_error("Window creation failed.");
+
+        while (!glfwWindowShouldClose(window))
+        {
+            glfwPollEvents();
+            glfwSwapBuffers(window);
+        }
+
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+    catch (std::runtime_error error) {
+        std::cerr << error.what() << std::endl;
+
+        throw;
+    }
+
+    std::cout << "done" << std::endl;
+
+    return 0;
+}
